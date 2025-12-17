a75759e7415d67d0ce1e8988b7d493d885797bc0
shayaan-1 <shayanbutt561@gmail.com>
2025-12-17 16:52:08 +0500

    fix2

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
-# VPC Outputs
+# VPC Output
 output "vpc_id" { value = module.vpc.vpc_id }
 output "public_subnet_ids" { value = module.vpc.public_subnet_ids }
 output "private_subnet_ids" { value = module.vpc.private_subnet_ids }
