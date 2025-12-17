db2ed9ecb0c8256de84cc15b63ddf1ab6d2678e3
shayaan-1 <shayanbutt561@gmail.com>
2025-12-17 16:30:14 +0500

    fixed

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "devops-terraform-state"
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-south-1"          
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
